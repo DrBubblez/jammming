df98cf250f92a2eaea862d076339f65b2b567585
Harry <harrisonminer59@gmail.com>
2025-10-08 12:27:22 -0400

    Theme & Styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,22 @@
 function App() {
   return (
-    <>
-      <h1>JAMMMING APP!</h1>
-    </>
+    <main className="bg-[url(/images/aleksandr-popov.jpg)] bg-cover bg-fixed min-h-screen grid grid-cols-1 grid-rows-9">
+      
+      <header className="bg-violet-950 content-center">
+        <h1 className="text-7xl text-center text-white font-[Bagel_Fat_One] tracking-wider text-shadow-lg/60 text-shadow-neutral-600">
+          <span>JA</span>
+          <span className="text-violet-500 text-shadow-violet-500">MMM</span>
+          <span>ING</span>
+        </h1>
+      </header>
+      <section>
+
+      </section>
+      <section>
+
+      </section>
+
+    </main>
   )
 }
 
