96ae0b3a76672576339da02176c96e1691a8f16a
Harry <harrisonminer59@gmail.com>
2025-10-08 18:26:43 -0400

    Small Update

---
+++ src/components/App/app-styles.js
@@ -0,0 +1,8 @@
+const appStyles = {
+    main: "bg-[url(/images/aleksandr-popov.jpg)] bg-cover bg-fixed min-h-screen grid grid-cols-1 grid-rows-9",
+    header: "bg-violet-950 content-center",
+    h1: "text-7xl text-center text-white font-[Bagel_Fat_One] tracking-wider text-shadow-lg/60 text-shadow-neutral-600",
+    purpleSpan: "text-violet-500 text-shadow-violet-500"
+};
+
+export default appStyles;
